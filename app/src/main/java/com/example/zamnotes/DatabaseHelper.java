3df758636b7194398fbaed18408d056b7c6b2559
LaGapaz <gapasin.marc@yahoo.fr>
2021-03-24 11:45:55 +0100

    Création de la BDD avec onCreate et onUpgrade

--- app/src/main/java/com/example/zamnotes/DatabaseHelper.java
+++ app/src/main/java/com/example/zamnotes/DatabaseHelper.java
@@ -1,6 +1,40 @@
 package com.example.zamnotes;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Nom de la Table :
+    public static final String TABLE_NAME = "NOTES";
+
+    // Colonnes :
+    public static final String _ID = "_id";
+    public static final String TITRE = "titre";
+    public static final String DESC = "contenu";
+
+    // BDD :
+    static final String DB_NAME = "ZAM_APP.DB";
+
+    // DB Version :
+    static final int DB_VERSION = 1;
+
+    // Création de la Table :
+    private static final String CREATE_TABLE = "create table" + TABLE_NAME + "(" + _ID  + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TITRE + " TEXT NOT NULL, " + DESC + " TEXT);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, factory:null, DB_VERSION);
+    }
+
+    @Override
+    public void OnCreate(SQLiteDatabase db){
+        // Execute la requete
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int OldVersion, int NewVersion) {
+        db.execSQL("DROP TABLE UF EXITST " + TABLE_NAME);
+        onCreate(db);
+    }
 }
