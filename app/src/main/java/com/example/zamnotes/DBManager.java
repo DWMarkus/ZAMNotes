b4d642fd20076f12cb4c0850b07e36ae98eed500
Marc Gapasin <gapasin.marc@yahoo.fr>
2021-04-07 10:23:58 +0200

    Méthodes insert, update, delete et fetch

--- app/src/main/java/com/example/zamnotes/DBManager.java
+++ app/src/main/java/com/example/zamnotes/DBManager.java
@@ -38,4 +38,43 @@ public class DBManager {
         contentValues.put(DatabaseHelper.DESC, desc);
         database.insert(DatabaseHelper.TABLE_NAME, null, contentValues);
     }
+
+    public Cursor fetch()
+    {
+        String[] columns = new String[] {DatabaseHelper._ID,
+                DatabaseHelper.TITRE, DatabaseHelper.DESC};
+
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME, columns,
+                selection(null),
+                selectionArgs(null),
+                groupBy(null),
+                having(null),
+                orderBy(null)
+        );
+
+        if (cursor != null)
+        {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+
+    private int update(long _id, String name, String desc)
+    {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.SUBJECT, name);
+        contentValues.put(DatabaseHelper.DESC, desc);
+
+        int i = database.update(DatabaseHelper.TABLE_NAME,
+                contentValues, DatabaseHelper._ID + " = " + _id, null);
+        return i;
+    }
+
+
+    public void delete(long _id)
+    {
+        database.delete(DatabaseHelper.TABLE_NAME,
+                DatabaseHelper._ID + " = " + _id, null);
+    }
 }
