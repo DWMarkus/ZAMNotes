bfaee9f355fab9a8ca576ae6ae6727521664f53d
Marc Gapasin <gapasin.marc@yahoo.fr>
2021-04-07 09:36:34 +0200

    Ajout des méthodes de DBManager

--- app/src/main/java/com/example/zamnotes/DBManager.java
+++ app/src/main/java/com/example/zamnotes/DBManager.java
@@ -1,9 +1,41 @@
 package com.example.zamnotes;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 
 public class DBManager {
 
     private DatabaseHelper dbHelper;
     private Context context;
+    private SQLiteDatabase database;
+
+
+    /// Construteur
+    public DBManager(Context c)
+    {
+        context = c;
+    }
+
+    public DBManager open() throws SQLiteException
+    {
+        dbHelper = new DatabaseHelper(contenu);
+        database = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close()
+    {
+        dbHelper.close();
+    }
+
+    // Méthodes de DBManager
+    public void insert(String name, String desc)
+    {
+        ContentValues contentValues = new ContextValues();
+        ContentValues.put(DatabaseHelper.TITRE, name);
+        ContentValues.put(DatabaseHelper.DESC, desc);
+        database.insert(DatabaseHelper.TABLE_NAME, value: null, contentValues);
+    }
 }
